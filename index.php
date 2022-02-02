89f9b0b25966e6c5cdce24a0fc74ec22df263f9e
andrey1303110 <andrey1303110@gmail.com>
2022-02-02 15:14:50 +0200

    add function for set params (tests)

--- index.php
+++ index.php
@@ -1,5 +1,15 @@
 <?php
-    include_once('mysqli_def.php')
+    include_once('mysqli_def.php');
+
+    function set_params() {
+        if (isset($_GET['params'])) {
+            $params = explode( "/", $_GET['params']);
+            $city = $params[0];
+            $donut = $params[1];
+        }
+    }
+
+    set_params();
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -8,19 +18,32 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Testing htaccess</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="/style.css">
 </head>
 <body>
     <?php
-        echo "<nav>";
+        $mysqli->connect_errno ? print('Connection error!') : print('Connection to DB success!');
+
         $cities = $mysqli->query("SELECT * FROM cities");
+        echo "<nav>";
         while ($row = mysqli_fetch_assoc($cities)) {
             echo "<a href=/".$row['city_name']."><div>";
                 echo "<p>".$row['city_name']."</p>";
             echo "</div></a>";
         }
         echo "</nav>";
-        var_dump($_GET);
+
+        function city_print($str) {
+            echo "<div><p>Your city is ". $str."</p>";
+        }
+
+        function donut_print($str) {
+            echo "<div><p>Your donut is ". $str."</p>";
+            echo "<img src=".strtolower($str).">";
+        }
+
+        $city ? city_print($city) : false;
+        $donut ? donut_print($donut) : false;
     ?>
 </body>
 </html>
