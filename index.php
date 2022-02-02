8a564d96375031436e59a974ea2624a97ba2ac8e
andrey1303110 <andrey1303110@gmail.com>
2022-02-02 14:02:01 +0200

    some fix

--- index.php
+++ index.php
@@ -11,15 +11,16 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <nav>
-        <?php
-            $cities = $mysqli->query("SELECT * FROM cities");
-            while ($row = mysqli_fetch_assoc($cities)) {
-                echo "<a href=/".$row['city_name']."><div>";
-                    echo "<p>".$row['city_name']."</p>";
-                echo "</div></a>";
-            }
-        ?>
-    </nav>
+    <?php
+        echo "<nav>";
+        $cities = $mysqli->query("SELECT * FROM cities");
+        while ($row = mysqli_fetch_assoc($cities)) {
+            echo "<a href=/".$row['city_name']."><div>";
+                echo "<p>".$row['city_name']."</p>";
+            echo "</div></a>";
+        }
+        echo "</nav>";
+        var_dump($_GET);
+    ?>
 </body>
 </html>
