316f9b40bdb23673bfe1a5a9ef96b8db2e80b5f0
andrey1303110 <andrey1303110@gmail.com>
2022-02-02 13:42:12 +0200

    add values from DB for test

--- index.php
+++ index.php
@@ -8,16 +8,16 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Testing htaccess</title>
-
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <nav>
         <?php
             $cities = $mysqli->query("SELECT * FROM cities");
             while ($row = mysqli_fetch_assoc($cities)) {
-                echo "<div>";
-                echo "<a href=/".$row['city_name'].">".$row['city_name']."</a>";
-                echo "</div>";
+                echo "<a href=/".$row['city_name']."><div>";
+                    echo "<p>".$row['city_name']."</p>";
+                echo "</div></a>";
             }
         ?>
     </nav>
