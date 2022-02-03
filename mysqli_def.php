7fb064e6bf3bb1c1d1860fc6c01bbe5e72a6a484
andrey1303110 <andrey1303110@gmail.com>
2022-02-03 10:30:53 +0200

    change connect to DB

--- mysqli_def.php
+++ mysqli_def.php
@@ -1,8 +1,8 @@
 <?php
 $host = 'localhost';
-$user_DB = 'test';
-$pass = 'test';
-$DB = 'test';
+$user_DB = 'devcot';
+$pass = 'Kxdi3gwgq2kBWJ9i';
+$DB = 'devcot_db1';
 
 $mysqli = mysqli_connect($host, $user_DB, $pass, $DB);
 $mysqli->connect_errno ? print('Connection error!') : false;
